ab16668755f91bdd3ba36e09a9e485a14d199412
natashamorais <natasha.sousa.morais@gmail.com>
2018-09-12 16:43:03 -0300

    exercicio dia 2 js

--- loops/01.js
+++ loops/01.js
@@ -53,6 +53,11 @@ for (i = 1; i <= 249; i++){
   console.log (`multi ${multi}`)
 }
 
+let soma = 0
+for (let i = 2; i <= 250; i++) {
+	soma += i * (i - 1)
+}
+
 // Escreva um loop em Javascript que
 // vai calcular 10! (10 fatorial), o
 // que significa 10 * 9 * 8 * 7 ... * 1.
@@ -90,12 +95,36 @@ while ( soma <= 1000000){
 // Desta forma, por exemplo, se a sequência 
 // iniciar com o número 5 ter-se-á: 5; 16; 8; 4; 2; 1".
 
+let collatz =  5 
+
+while (collatz > 1){
+  if (collatz % 2 === 0){
+    collatz = collatz/2
+  }else{
+    collatz = collatz *3+1
+  }
+  console.log (collatz)
+}
 
 
 
 // Escreva um loop em Javascript
 // que gera uma lista com 100
 // número randomicos.
+let arr = []
+for ( i = 0; i<100; i++){
+console.log (Math.random ())
+}
+for ( i = 0; i<100; i++){
+  arr.push (Math.random ())
+}
+
+let arr = []
+while ( arr.length > 100){
+
+  arr.push(Math.random ())
+}
+console.log(arr)
 
 
 
@@ -106,3 +135,36 @@ while ( soma <= 1000000){
 // 0 = coroa. Rode esse loop 1000 vezes e
 // printe o numero de caras e o numero
 // de coroas no console.
+let cara = 0
+let coroa = 0 
+
+for ( let i = 0; i < 1000; i++){
+  let moeda = Math.floor(Math.random() * 2)
+  if(moeda === 1 ){
+ cara++ // cara++
+  }else{
+coroa ++ // coroa= coroa+1
+  }
+}
+
+
+//resoluçao 
+let cara = 0
+let coroa = 0
+for (let i = 0; i < 1000; i++) {
+	let moeda = Math.floor(Math.random() * 2)
+	if (moeda === 1) {
+		cara++
+	} else {
+		coroa++
+	}
+}
+// ou...
+while (cara + coroa < 1000) {
+	let moeda = Math.floor(Math.random() * 2)
+	if (moeda === 1) {
+		cara++
+	} else {
+		coroa++
+	}
+}
--- loops/02.js
+++ loops/02.js
@@ -6,6 +6,14 @@
 //   [0, 0, 0, 0, 0],
 //   [0, 0, 0, 0, 0] ]
 
+for (matriz = 0; matriz < 5; matriz++) {
+    let arr = []
+    for (i = 0; i < 5; i++) {
+        arr.push("0")
+    }
+    console.log(arr)
+}
+
 
 
 
@@ -18,19 +26,78 @@
 //   [3, 2, 1, 0, 1],
 //   [4, 3, 2, 1, 0] ]
 
+//let arr = []
+//for ( i = 0; i < 5; i++){
+//     const arrDeDentro = [];k
+//     for ( matriz = 0; matriz < 5; matriz++){
+//         arrDeDentro.push(i)
+//     }
+//     arr.push(arrDeDentro)
+// }
 
 
-
-// Escreva um loop em Javascript
+for (matriz = 0; matriz < 5; matriz++) {
+    let arr = []
+    for (i = 0; i < 5; i++) {
+        arr.push(matriz)
+        let subtrair = Math.abs(matriz - i)
+    }
+    console.log(arr)
+}
+// creva um loop em Javascriptb 
 // que printa o seguinte padrão
 // no console:
-// *******
+// **** ***
 // ******
 // *****
 // ****
 // ***
 // **
 // *
+ let all=[]
+for (m = 0; m > 7; m++){
+let arr= []
+for ( i =0; i > 7 - m; i++){
+    arr.push("*")
+    }
+  all.push(arr) 
+ 
+}
+console.log (all)
+
+for (let j = 0; j < 7; j++) { 
+    let asteriscos = ""
+    for (let i = 0; i < (7 - j); i++) { 
+        asteriscos += "*"
+    }
+    console.log(asteriscos)
+}
+// ou...
+for (let j = 0; j < 7; j++) { 
+    let asteriscos = ""
+    for (let i = j; i < 7; i++) { 
+        asteriscos += "*"
+    }
+    console.log(asteriscos)
+}
+// ou...
+let asteriscos = "********"
+while (asteriscos.length > 1) {
+    asteriscos = asteriscos.substr(1, (asteriscos.length - 1))
+    console.log(asteriscos)
+}
+// ou...
+let asteriscos = ["*", "*", "*", "*", "*", "*", "*", "*"]
+for (let i = 7; i < 7; i++) {
+    asteriscos = asteriscos.slice(1)
+    console.log(asteriscos.join(""))
+}
+// ou...
+let asteriscos = ["*", "*", "*", "*", "*", "*", "*", "*"]
+for (let i = 0; i < 7; i++) {
+    asteriscos.pop() 
+    console.log(asteriscos.join(""))
+}
 
 
 
@@ -45,3 +112,24 @@
 // 12345**
 // 123456*
 // 1234567 
+
+for (let j = 1; j <= 7; j++) {
+    let linha = ""
+    for (let k = 1; k <= j; k++) {
+        linha += k
+    }
+    for (let i = j; i < 7; i++) { 
+        linha += "*"
+    }
+    console.log(linha)
+}
+// ou...
+let digitos = ""
+for (let i = 1; i <= 7; i++) {
+    digitos += i
+    let numero_com_asteriscos = digitos
+    for (let j = 7; j > i; j--) {
+        numero_com_asteriscos += "*"
+    }
+    console.log(numero_com_asteriscos)
+} 
